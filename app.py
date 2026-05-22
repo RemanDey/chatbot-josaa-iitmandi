2d63d0c0a4eb578ac9fdeb4b8ec385d69a85bac4
Reman Dey <reman.airport@gmail.com>
2026-05-23 00:34:58 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -118,4 +118,8 @@ def send_whatsapp_message(to_phone, reply_text):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+   
+    port = int(os.environ.get("PORT", 5000))
+    
+
+    app.run(host="0.0.0.0", port=port)
